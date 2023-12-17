2afbf5218c1143c4fc5450ad7398efa33ac9758d
Yulia Lapshina <109868318+dymonyx@users.noreply.github.com>
2023-12-17 04:48:35 +0300

    Add new function - create task

--- task_manager.py
+++ task_manager.py
@@ -1,5 +1,10 @@
 tasks = []
 
+def create_task(title, description):
+    task = {"title": title, "description": description, "status": "ToDo"}
+    tasks.append(task)
+    print(f"Создана новая задача: {title}")
+
 def get_all_tasks():
     return tasks
 
@@ -16,7 +21,10 @@ def update_task_status(title, new_status):
 
 
 action = input("Что вы хотите сделать? Создать, Посмотреть все таски, Изменить статус: ")
-if action == "Посмотреть все таски":
+
+if action == "Создать":
+  create_task(input("Ввведите название: "), input("Ввведите описание: "))
+elif action == "Посмотреть все таски":
   print(get_all_tasks)
 else:
   update_task_status(input("Ввведите название: "), input("Введите новый статус: ")
